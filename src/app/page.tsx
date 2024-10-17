df73f614bc5d6841fe9afd8a42feebc9f7ad570b
jlbavaresco@gmail.com <jlbavaresco@gmail.com>
2024-10-17 16:40:47 -0300

    deixando o compoennte home como client para não dar erro no deploy da vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,25 @@
-'use client'
 import Produto from "@/bd/entitites/Produto";
 import { getProdutosAPI } from "@/servicos/ProdutoServico";
 import CardProduto from "@/componentes/CardProduto";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
-//export const revalidate = 60; // revalida a cada 60 segundos
+export const revalidate = 60; // revalida a cada 60 segundos
 
 export default async function Home() {
 
-  //const produtos = await getProdutosAPI();
+  const produtos = await getProdutosAPI();
 
   return (
     <div style={{ padding: '20px' }}>      
       <Container>
-{/*         <Row>
+         <Row>
           {
             produtos.map((produto: Produto) => (             
                 <CardProduto produto={produto} key={produto.codigo}/>           
             ))
           }
-        </Row> */}
+        </Row>
       </Container>
     </div>
   );
