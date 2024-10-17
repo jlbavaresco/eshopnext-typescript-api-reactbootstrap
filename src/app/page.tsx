24ab8be2352f007d4a5e01d17c1620522a758e79
jlbavaresco@gmail.com <jlbavaresco@gmail.com>
2024-10-17 16:21:55 -0300

    ajustes para revalidar a home de tempos em tempos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import { getProdutosAPI } from "@/servicos/ProdutoServico";
 import CardProduto from "@/componentes/CardProduto";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+
+export const revalidate = 60; // revalida a cada 60 segundos
+
 export default async function Home() {
 
   const produtos = await getProdutosAPI();
